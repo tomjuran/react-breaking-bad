8bbb9ef3ea4be292be29417cc8e6b2e380ac27a5
tomjuran <68550775+tomjuran@users.noreply.github.com>
2020-10-15 09:39:32 -0500

    Add files via upload

---
+++ src/components/ui/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../../img/logobr.png'
+
+const header = () => {
+    return (
+        <header className='center'>
+            <img src={logo} alt=''></img>
+       
+        </header>
+    )
+}
+
+export default header
